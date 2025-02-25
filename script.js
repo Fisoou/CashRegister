a617558a9d4c68ca34f33349faddee5714b8b2d8
Fisou <fisou69@gmail.com>
2025-02-25 09:42:36 +0300

    silly little interactive buttons

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const input = document.getElementById("user-input");
+const submit = document.getElementById("purchase-btn");
+const buttons = document.querySelectorAll(".buttons button");
+
 let price = 1.87;
 const cash = 0;
 let cid = [
@@ -11,3 +15,19 @@ let cid = [
   ["TWENTY", 60],
   ["ONE HUNDRED", 100],
 ];
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const number = button.textContent;
+    if (!isNaN(number)) {
+      console.log(number);
+      input.value += number;
+    } else if (number === "✓") {
+      calculate();
+    } else if (number === "✗") {
+      input.value = "";
+    }
+  });
+});
+
+const calculate = () => {};
